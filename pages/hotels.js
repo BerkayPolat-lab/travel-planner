7b60c36fa7f6471051804cdfe28d674ca09cf161
Mert Bildirici <hmertbildirici@gmail.com>
2025-02-17 16:21:24 -0500

    will add hotels.com api

--- pages/hotels.js
+++ pages/hotels.js
@@ -1,12 +1,24 @@
-
-
+import { useState } from 'react';
 
 const Hotels = () => {
+    const [city, setCity] = useState('');
+
+    const handleInputChange = (event) => {
+        setCity(event.target.value);
+    };
+
     return (
         <div>
             <h1>Hotels</h1>
+            <input 
+                type="text" 
+                placeholder="Enter city" 
+                value={city} 
+                onChange={handleInputChange} 
+            />
+            <p>You are traveling to: {city}</p>
         </div>
-    )
+    );
 }
 
 export default Hotels;
